03b813334837d819f4d5779583f33427949515ec
Emilia Wróblewska <emiliamwroblewska@gmail.com>
2021-01-24 17:09:37 +0100

    Final solution

--- classify.py
+++ classify.py
@@ -7,21 +7,19 @@ import shutil
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
 
 from skimage import feature, filters
+from skimage.transform import resize
 from skimage.io import imread
 from pylab import imshow, show 
 from matplotlib import pyplot as plt
 import matplotlib
-# XGBClassifier
-# RandomForestClassifier
-# DecisionTreeClassifier
-# SGDClassifier
 
-images_per_class = 20000
+### Variables ###
+images_per_class = 1000
 train_path = "Data"
 bins = 8
 num_trees = 100
@@ -29,8 +27,8 @@ test_size = 0.20
 seed = 9
 scoring = "accuracy"
 
-# Feature Descriptors
-# 1: Hu moments
+### Feature Descriptors ###
+# 1: Hu Moments
 def fd_hu_moments(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     hu = cv2.HuMoments(cv2.moments(image)).flatten()
@@ -50,7 +48,7 @@ def fd_histogram(image, mask=None):
     return hist.flatten()
 
 # 4: Local Binary Pattern (LBP)
-def fd_local_binary_pattern(image, numPoints=24, radius=8, eps=1e-7):
+def fd_lbp(image, numPoints=24, radius=8, eps=1e-7):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     lbp = feature.local_binary_pattern(image, numPoints, radius, method="uniform")
     hist, _ = np.histogram(lbp.ravel(), bins=np.arange(0, numPoints + 3), range=(0, numPoints + 2))
@@ -58,74 +56,87 @@ def fd_local_binary_pattern(image, numPoints=24, radius=8, eps=1e-7):
     hist /= (hist.sum() + eps)
     return hist
 
-# 5: Canny Edge Detector
-def fd_canny(image): 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    canny = feature.canny(image)
-    return canny.flatten()
-   
-# 6: Sobel Edge Detector
-def fd_sobel(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    sobel = filters.sobel(image)
-    return sobel.flatten()
+# 5: Histogram of Oriented Gradients (HOG)
+def fd_hog(image):
+    image = resize(image, (128, 64))
+    fd = feature.hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, multichannel=True)
+    return fd
+
+# 6: Zernike Moments
+def fd_zernike(image):
+    return mahotas.features.zernike_moments(image[:, :, 0], degree=20, radius=20)
 
 
-
-# 
-# class_labels = os.listdir(train_path)
-# features_list = []
-# labels = []
-
-# # loop over the training data sub-folders
-# for class_name in class_labels:
-#     # join the training data path and each species training folder
-#     dir = os.path.join(train_path, class_name)
-
-#     # get the current training label
-#     current_label = class_name
-
-#     # loop over the images in each sub-folder
-#     for x in range(1, images_per_class+1):
-
-#         file = dir + "/" + str(x) + ".jpg"
-
-#         image = cv2.imread(file)
-
-#         # Global Feature extraction
-#         fv_hu_moments = fd_hu_moments(image)
-#         fv_haralick = fd_haralick(image)
-#         fv_histogram = fd_histogram(image)
-
-        
-#         feature = np.hstack([fv_histogram, fv_haralick, fv_hu_moments])
-
-#         # update the list of labels and feature vectors
-#         labels.append(current_label)
-#         features_list.append(feature)
-
-#     print("[STATUS] processed folder: {}".format(current_label))
-
-# print("[STATUS] completed Global Feature Extraction...")
+### Run during first execution of program to ensure consistency in files' naming ###
+# path = './Data/Positive'
+# files = os.listdir(path)
+# for f in files:
+#     if '_1' in f:
+#         newname = f.replace('_1','')
+#         os.rename(os.path.join(path, f),os.path.join(path,newname))
 
 
-# # encode the target labels
-# le          = LabelEncoder()
-# target      = le.fit_transform(labels)
-# # scale features in the range (0-1)
-# scaler            = MinMaxScaler(feature_range=(0, 1))
-# rescaled_features = scaler.fit_transform(global_features)
+### Classification ###
+class_labels = os.listdir(train_path)
+features_list = []
+labels = []
 
-# #initiate ML model
-# clf  = RandomForestClassifier(n_estimators=num_trees, random_state=seed)
+# Iterate over training folders
+for class_name in class_labels:
+    dir = os.path.join(train_path, class_name)
+    current_label = class_name
+    print("\nExtracting features in folder: {} ...".format(current_label))
+    for x in range(1, images_per_class+1):
+        if x < 10:
+            file = dir + "/0000" + str(x) + ".jpg"
+        elif 9 < x < 100:
+            file = dir + "/000" + str(x) + ".jpg"
+        elif 99 < x < 1000:
+            file = dir + "/00" + str(x) + ".jpg"
+        elif 999 < x < 10000:
+            file = dir + "/0" + str(x) + ".jpg"
+        else:
+            file = dir + "/" + str(x) + ".jpg"
 
-# #initiate 10-fold validation function
-# kfold = KFold(n_splits=10, random_state=seed)
+        # All files are the same size so no need for resizing
+        image = cv2.imread(file)
+        # Feature Extraction
+        fv_hu_moments = fd_hu_moments(image)
+        fv_haralick = fd_haralick(image)
+        fv_histogram = fd_histogram(image)
+        fv_lbp = fd_lbp(image)
+        fv_hog = fd_hog(image)
+        fv_zernike = fd_zernike(image)
 
-# #predict the images labels using model with input data and 10-fold validation
-# cv_results = cross_val_score(clf, rescaled_features, target, cv=kfold, scoring=scoring)
-# print(cv_results.mean())
+        cumulative_feature = np.hstack([fv_haralick,fv_lbp,fv_hog])
+        # cumulative_feature = np.hstack([fv_hu_moments])
+        # cumulative_feature = np.hstack([fv_haralick])
+        # cumulative_feature = np.hstack([fv_histogram])
+        # cumulative_feature = np.hstack([fv_lbp])
+        # cumulative_feature = np.hstack([fv_hog])
+        # cumulative_feature = np.hstack([fv_zernike])
 
+        # Update the list of labels and feature vectors
+        labels.append(current_label)
+        features_list.append(cumulative_feature)
+    print("Processed folder: {}".format(current_label))
+print("Feature Extraction Completed.\n")
 
+# Encode the target labels
+encoder = LabelEncoder()
+target = encoder.fit_transform(labels)
+# Scale features in the range (0-1)
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaled_features = scaler.fit_transform(features_list)
+print("Encoded labels, scaled features.\nTraining the model...")
 
+# Initiate ML model and divide dataset into training and testing part
+clf = RandomForestClassifier(n_estimators=num_trees, random_state=seed)
+(trainFeatures, testFeatures, trainLabels, testLabels) = train_test_split(np.array(rescaled_features),np.array(target),test_size=test_size,random_state=seed)
 
+# Train the model and evaluate prediction score
+clf.fit(trainFeatures, trainLabels)
+result = clf.score(testFeatures, testLabels)
+
+print("\nImages per class used: {}".format(images_per_class))
+print("Accuracy for Random Forest Classifier: {}\n".format(result))
--- visualize.py
+++ visualize.py
@@ -3,13 +3,14 @@ import mahotas
 import cv2
 import os
 
-from skimage import feature, filters
+from skimage import feature, filters, exposure
 from skimage.io import imread
+from skimage.transform import resize
 from pylab import imshow, show 
 from matplotlib import pyplot as plt
 import matplotlib
 
-# ---------------------------------------
+# -------------------------------------------
 numPoints = 24
 radius = 8
 
@@ -23,15 +24,16 @@ nh_gray = mahotas.features.haralick(ngray)
 ph_gray = mahotas.features.haralick(pgray) 
 nh = mahotas.features.haralick(imNeg)
 ph = mahotas.features.haralick(imPos)
+
 fig, ((f1,f2), (f3,f4)) = plt.subplots(nrows=2, ncols=2,figsize=(9, 6))
 f1.imshow(nh_gray)
-f1.set_xlabel('Haralick (on grayscale) Negative')
+f1.set_xlabel('Haralick on grayscale image (Negative)')
 f2.imshow(ph_gray)
-f2.set_xlabel('Haralick (on grayscale) Positive')
+f2.set_xlabel('Haralick on grayscale image (Positive)')
 f3.imshow(nh)
-f3.set_xlabel('Haralick (on original) Negative')
+f3.set_xlabel('Haralick on original image (Negative)')
 f4.imshow(ph)
-f4.set_xlabel('Haralick (on original) Positive')
+f4.set_xlabel('Haralick on original image (Positive)')
 plt.show()
 
 # Color Histogram 
@@ -41,7 +43,7 @@ for i,col in enumerate(color):
     histr = cv2.calcHist([imHSV],[i],None,[256],[0,256])
     plt.plot(histr,color = col)
     plt.xlim([0,256])
-    plt.title('Color histogram for Negative HSV photo (without crack)')
+    plt.title('Color Histogram for image in HSV (Negative)')
 plt.show()
 
 imHSV_P = cv2.cvtColor(imPos,cv2.COLOR_BGR2HSV)
@@ -50,9 +52,8 @@ for i,col in enumerate(color):
     histr = cv2.calcHist([imHSV_P],[i],None,[256],[0,256])
     plt.plot(histr,color = col)
     plt.xlim([0,256])
-    plt.title('Color histogram for Positive HSV photo (with crack)')
+    plt.title('Color Histogram for image in HSV (Positive)')
 plt.show()
-
 
 # Local Binary Pattern (LBP)
 lbpN = feature.local_binary_pattern(ngray, numPoints, radius, method="uniform")
@@ -60,9 +61,9 @@ lbpP = feature.local_binary_pattern(pgray, numPoints, radius, method="uniform")
 fig, ((ax1,ax2), (ax3,ax4)) = plt.subplots(nrows=2, ncols=2,figsize=(9, 6))
 plt.gray()
 ax1.imshow(imNeg)
-ax1.set_xlabel('Image without Crack')
+ax1.set_xlabel('Image without Crack (Negative)')
 ax2.imshow(imPos)
-ax2.set_xlabel('Image with Crack')
+ax2.set_xlabel('Image with Crack (Positive)')
 
 ax3.hist(lbpN.ravel(), density=True, bins=numPoints + 2, range=(0, numPoints + 2))
 ax3.set_ylabel('Percentage')
@@ -72,24 +73,60 @@ ax4.set_ylabel('Percentage')
 ax4.set_xlabel('Uniform LBP values')
 plt.show()
 
-# Canny edge detection
-cannyN = feature.canny(ngray)
-cannyP = feature.canny(pgray)
-fig, (c1,c2) = plt.subplots(nrows=1, ncols=2,figsize=(9, 6))
-c1.imshow(cannyN,cmap='gray')
-c1.set_xlabel('Negative (without crack)')
-fig.suptitle('Canny Edge detection', fontsize=16)
-c2.imshow(cannyP,cmap='gray')
-c2.set_xlabel('Positive (with crack)')
+# Histogram of Oriented Gradients
+res_imgN = resize(imNeg, (128, 64))
+res_imgP = resize(imPos, (128, 64))
+fdN, hog_imageN = feature.hog(res_imgN, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True, multichannel=True)
+fdP, hog_imageP = feature.hog(res_imgP, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True, multichannel=True)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8), sharex=True, sharey=True) 
+hog_image_rescaled = exposure.rescale_intensity(hog_imageN, in_range=(0, 10)) 
+ax1.imshow(hog_image_rescaled, cmap=plt.cm.gray) 
+ax1.set_title('Negative') 
+fig.suptitle('Histogram of Oriented Gradients', fontsize=16)
+hog_image_rescaledP = exposure.rescale_intensity(hog_imageP, in_range=(0, 10)) 
+ax2.imshow(hog_image_rescaledP, cmap=plt.cm.gray) 
+ax2.set_title('Positive')
+
 plt.show()
 
-# Sobel Edge detection
-sobelN = filters.sobel(ngray)
-sobelP = filters.sobel(pgray)
-fig, (s1,s2) = plt.subplots(nrows=1, ncols=2,figsize=(9, 6))
-s1.imshow(sobelN,cmap='gray')
-s1.set_xlabel('Negative (without crack)')
-fig.suptitle('Sobel Edge detection', fontsize=16)
-s2.imshow(sobelP,cmap='gray')
-s2.set_xlabel('Positive (with crack)')
+# Zernike Moments
+filtN = imNeg[:,:,0]
+filtP = imPos[:,:,0]
+fig, (zer1, zer2) = plt.subplots(1, 2, figsize=(12, 6), sharex=True, sharey=True)
+zer1.imshow(filtN) 
+zer1.set_title('Negative') 
+fig.suptitle('Filterd Images for Zernike Moments', fontsize=18)
+zer2.imshow(filtP) 
+zer2.set_title('Positive')
+plt.show()
+
+# --------------------------------------------------
+# Plotting accuracy as a function of sample size
+# Hu Moments
+x = np.linspace(1, 20, 20)
+y = np.array([91.75,93.875,92,92.31,92,92,92.32,92.28,92.64,93.425,93.41,92.94,92.885,93.375,93.18,93.81,93.985,94.46,94.355,94.4])
+
+plt.plot(x, y, 'o', color='blue'); 
+plt.xlim([0,21])
+plt.ylim([0,100])
+plt.xticks(np.arange(0, 21, 1))
+plt.grid(True)
+plt.xlabel('Sample size (in images per class *1000)')
+plt.ylabel('Accuracy (in %)')
+plt.title('Accuracy as function of sample size - Hu Moments')
+plt.show()
+
+# Zernike Moments
+x = np.linspace(1, 20, 20)
+y = np.array([74.25,78.625,79.58,80.5,80.75,78.54,79.07,78.97,80.19,79.65,80,80.71,80.44,80.41,79.45,80.41,80.93,81.72,81.37,82.65])
+
+plt.plot(x, y, 'o', color='green'); 
+plt.xlim([0,21])
+plt.ylim([0,100])
+plt.xticks(np.arange(0, 21, 1))
+plt.grid(True)
+plt.xlabel('Sample size (in images per class *1000)')
+plt.ylabel('Accuracy (in %)')
+plt.title('Accuracy as function of sample size - Zernike Moments')
 plt.show()
